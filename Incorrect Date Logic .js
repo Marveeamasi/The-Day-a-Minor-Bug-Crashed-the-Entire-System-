e5a14e518865b70a8e01e098c87cd1df649cc9e6
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-12-20 05:35:06 +0100

    Update Incorrect Date Logic .js

--- Incorrect Date Logic .js
+++ Incorrect Date Logic .js
@@ -1,1 +1,11 @@
-const renewalDate = new Date(user.subscriptionStartDate + 30);
+function calculateRenewalDate(subscriptionStartDate) {
+  const startDate = new Date(subscriptionStartDate);
+  
+  const renewalDate = new Date(startDate.getTime() + 30 * 24 * 60 * 60 * 1000);
+
+  return renewalDate;
+}
+
+const subscriptionStartDate = "2024-01-01";
+const renewalDate = calculateRenewalDate(subscriptionStartDate);
+console.log("Next renewal date:", renewalDate);
